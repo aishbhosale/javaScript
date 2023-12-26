c20cf8af620852df520be89446b7ad3be560d720
aishbhosale <aishwaryabhosale84@gmail.com>
2023-12-26 21:55:04 +0530

    created file 01_variableAssign using javaScript
    created file 01_variableAssign using javaScript

---
+++ 01_variableAssign.js
@@ -0,0 +1,34 @@
+
+
+var myfullName;
+console.log(myfullName);
+
+myfullName= "Aishwarya Bhosale ";
+console.log("My First Name & Last Name =", myfullName);
+
+myfullName= "Aishwarya Jayendra Bhosale";
+console.log("My Full Name =",myfullName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var fname="Aishwarya"
+console.log("First Name =", fname );
+
+var lname="Bhosale"
+console.log("Last Name =",lname);
+
+var age="25"
+console.log("Age =",age);
+
+var address="Katraj, Pune"
+console.log("Address =",address);
+
+var pin="411046"
+console.log("Pincode =",pin);
+
+var gender="Female"
+console.log("Gender =",gender);
+
+var marry="No"
+console.log("Married =",marry);
