4f810dd162ddafd13dbb44df9119360c4156aaca
aishbhosale <aishwaryabhosale84@gmail.com>
2024-01-13 00:42:54 +0530

    Created file for if else statement and ternary operator
    Created file for if else statement and ternary operator

--- 03_if else statement file-06.js
+++ 03_if else statement file-06.js
@@ -2,19 +2,22 @@
 
 
  function interview(gradScore,hscScore,sscScore,candidateName) {
+  // console.log(`Grade=${gradScore} : HSC=${hscScore} : SSC= ${sscScore}`);
 
-   if (gradScore <=70 || hscScore <=80 || sscScore <=90) {
+   if (gradScore >=70 || hscScore >=80 || sscScore >=90) {
 
-        console.log (` Congrates ${candidateName} you are eligible for TCS interview`);
+        console.log (`Congrates ${candidateName} you are eligible for TCS interview`);
 
    } else {
 
-      console.log (`Unfortunately  you are not eligible for interview`)
+      console.log (`Unfortunately ${candidateName} you are not eligible for TCS interview`);
     }
     
 }
 
  interview(80, 86 ,90, "Aishwarya")
- interview(69, 65, 55, "Deepali")
+ interview(70, 65, 55, "Priti")
+ interview(60,79,88, "Deepali")
 
 
+ 
