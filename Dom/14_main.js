8e7962e3d611042466f33fa55435901424582940
aishbhosale <aishwaryabhosale84@gmail.com>
2024-01-24 23:43:39 +0530

    Program of 14_dom Assignment.html
    Program of 14_dom Assignment.html

---
+++ Dom/14_main.js
@@ -0,0 +1,11 @@
+
+const headline= document.getElementById("head");
+console.log(headline);
+headline.style.color="darkblue";
+
+const headline2= document.getElementById("head2");
+console.log(headline2);
+headline2.style.color="red";
+
+const projectName = document.querySelector("#project");
+projectName.innerHTML = "Project of college Management System";
